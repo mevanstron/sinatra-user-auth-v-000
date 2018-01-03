de9b8f9e3fc2604e94cdb857c03e0bedabbd8162
Michael Evans <michael@michaelgregoryevans.com>
2018-01-03 21:08:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,8 +37,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-    binding.pry
-    @user = User.find(session[:id])
+    #@user = User.find(session[:id])
     erb :'/users/home'
   end
 
